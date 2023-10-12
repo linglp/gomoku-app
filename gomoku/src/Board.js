3e126d5e6d0fd361c6cf7740f65aea581ae5af3a
linglp <linglp@umich.edu>
2023-10-12 15:46:14 -0400

    add styling and create board

---
+++ gomoku/src/Board.js
@@ -0,0 +1,128 @@
+import Square from './Square.js'
+
+export default function Board() {
+    return (
+      <>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="row-board">
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </>
+    );
+  }
---
+++ gomoku/src/Square.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Square() {
+    const [value, setValue] = useState(null);
+
+
+    function handleClick(){
+        setValue('X')
+    }
+    return(
+        <button onClick={handleClick} className="square">{value}</button>
+    )
+    ;
+  }
+
+export default Square
